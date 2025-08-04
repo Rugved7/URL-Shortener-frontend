3c2dfffecd04bc07e942cd56ad825edf37646268
Rugved7 <rugvedagasti7@gmail.com>
2025-08-04 23:22:17 +0530

    Project initialised with basic tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl text-red-800'>
+        Hello , welcome to the URL Shortener!
+      </h1>
+    </div>
+  )
+}
+
+export default App
