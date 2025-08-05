c1522fe2bced319387e23006df22a152f3980f69
Rugved7 <rugvedagasti7@gmail.com>
2025-08-05 18:09:52 +0530

    Some key improvements in UI done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import LandingPage from "./components/LandingPage";
 import AboutPage from "./components/AboutPage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -8,10 +11,12 @@ const App = () => {
   return (
     <>
       <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
+      <Footer />
       </BrowserRouter>
     </>
   );
