2cc3dc2fb07f66fb2f28d4b4efbf7f37a50b7e43
Rugved7 <rugvedagasti7@gmail.com>
2025-08-05 17:40:03 +0530

    Basic Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import React from "react";
+import LandingPage from "./components/LandingPage";
+import AboutPage from "./components/AboutPage";
 
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl text-red-800'>
-        Hello , welcome to the URL Shortener!
-      </h1>
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
