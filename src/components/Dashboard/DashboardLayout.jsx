d76aed4af37d12887895779b27e6368b41ae8645
Rugved7 <rugvedagasti7@gmail.com>
2025-08-07 19:52:53 +0530

    Graph rendered for real users and clicks

--- src/components/Dashboard/DashboardLayout.jsx
+++ src/components/Dashboard/DashboardLayout.jsx
@@ -7,7 +7,8 @@ import { useFetchTotalClicks } from "../../hooks/useQuery";
 const DashboardLayout = () => {
   const { token } = useStoreContext();
 
-  console.log(useFetchTotalClicks(token, onError));
+  const {isLoading : loader, data : totalCicks} = useFetchTotalClicks(token, onError);
+
 
   function onError() {
     console.log("Error");
@@ -15,9 +16,24 @@ const DashboardLayout = () => {
 
   return (
     <div className="lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]">
+      {loader ? (
+        <p>Loading...</p>
+      ) : (
       <div className="lg:w-[90%] w-full mx-auto py-16">
         <div className="h-96 relative">
-          <Graph graphData={dummyData} />
+          {totalCicks.length == 0 && (
+             <div className="absolute flex flex-col  justify-center sm:items-center items-end  w-full left-0 top-0 bottom-0 right-0 m-auto">
+                     <h1 className=" text-slate-800 font-serif sm:text-2xl text-[18px] font-bold mb-1">
+                       No Data For This Time Period
+                     </h1>
+                     <h3 className="sm:w-96 w-[90%] sm:ml-0 pl-6 text-center sm:text-lg text-sm text-slate-600 ">
+                       Share your short link to view where your engagements are
+                       coming from
+                     </h3>
+                   </div>
+          )}
+          <Graph graphData={totalCicks} />
+
         </div>
 
         <div className="py-5 sm:text-end text-center">
@@ -26,6 +42,7 @@ const DashboardLayout = () => {
           </button>
         </div>
       </div>
+      )}
     </div>
   );
 };
