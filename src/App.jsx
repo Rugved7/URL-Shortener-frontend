4724f4a136c509d6b2e3fd5a6a77b5edc34fd87b
Rugved7 <rugvedagasti7@gmail.com>
2025-08-08 15:11:55 +0530

    Url Redirects successfull -> Short url redirected to long Urls

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
-import React from "react";
-import LandingPage from "./components/LandingPage";
-import AboutPage from "./components/AboutPage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import RegisterPage from "./components/RegisterPage";
-import LoginPage from "./components/LoginPage";
-import DashboardLayout from "./components/Dashboard/DashboardLayout";
-
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { getApps } from "./utils/helper";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
+  const CurrentApp = getApps();
   return (
-    <>
-      <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard/" element={<DashboardLayout />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    <Footer />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <CurrentApp/>
+    </BrowserRouter>
   );
 };
 
