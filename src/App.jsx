00b42719a8c9c51454a3942ef8ff2f7e98f5bde7
Rugved7 <rugvedagasti7@gmail.com>
2025-08-08 15:34:27 +0530

    Major Bug resolved-> App Working fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { getApps } from "./utils/helper";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   const CurrentApp = getApps();
   return (
     <BrowserRouter>
-      <CurrentApp/>
+      <CurrentApp />
     </BrowserRouter>
   );
 };
--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -3,4 +3,4 @@ import AppRouter, { subDomainRouter } from '../AppRouter';
 export const subDomainList = [
     {subdomain :"www", app: AppRouter,main: true},
     {subdomain :"url", app: subDomainRouter, main: false},
-]
+]   
